be0d1678b13a24d6e82db2b542d0e5a11d363025
Priyanka Lima <priyankalima99@gmail.com>
2022-05-24 10:19:53 +0000

    commit

--- api/index.js
+++ api/index.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const payment = require('./payment.js')
+// demo hellow rpwrld
+app.use('/',(req,res) =>{
+    res.send('hello wrold')
+})
+// payment routes
+app.use('/payment',payment)
 
+app.use(cors())
+
+app.listen('5000',function(){
+    console.log('server connected succesful')
+})
---
+++ api/payment.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+const Razorpay = require('razorpay');
+const crypto = require('crypto');
+const router = express.Router();
+
+// instance
+var instance = new Razorpay({ key_id: 'YOUR_KEY_ID', key_secret: 'YOUR_SECRET' })
+
+// create order
+router.post('/order', async(req, res) => {
+    var options = {
+        amount: 50000,  // amount in the smallest currency unit
+        currency: "INR",
+    };
+    const order = await instance.orders.create(options);
+    if(!order) return res.status(500).send('somethings went wrong')
+    return res.status.apply(200).json(order)
+})
+
+// verify and cature the payment 
+router.post('/verify',async(req,res)=>{
+    try{
+         const{
+             orderCreateId,
+             razorpayPaymentId,
+             razorpayOderId,
+             razorpaySignature,
+             amount,
+             currency
+         }= req.body
+         const signature = crypto.createHmac('sha256','secrete key');
+         signature.update(`${orderCreateId}|${razorpayPaymentId}`)
+         const digest = signature.digest('hex')
+         if(digest !== razorpaySignature ) return res.status(400).json({msg:'something bad error'})
+
+        //  capture the payment
+        const response ={
+            razorpayPaymentId,
+            amount,
+            currency
+        }
+        const captureResponse = await instance.payment.capture(response)
+        return res.status(200).json({
+            status:'success',
+            orderId:razorpayOderId,
+            paymentId:razorpayPaymentId,
+            captureResponse
+        })
+    }
+    
+    catch(err){
+        res.status(500).send('bad error')
+        console.log(err)
+    }
+})
+
+
+
+// export module router
+module.exports = router;
